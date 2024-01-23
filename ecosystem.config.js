db39db9a5b34fb01ac16d83bb548a27c20b87d18
Zero的Mac <1203970284@qq.com>
2024-01-24 05:23:31 +0800

    fix: pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,14 +4,14 @@ module.exports = {
       name: "NailyPassport",
       script: "./resources/dist/apps/passport/main.js",
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
     },
     {
       name: "NailyShop",
       script: "./resources/dist/apps/shop/main.js",
       env: {
-        NODE_ENV: "development",
+        NODE_ENV: "production",
       },
     },
   ],
