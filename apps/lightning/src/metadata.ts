5a75512d3847f0041d198d6c238ac73a128445dd
Zero的Mac <1203970284@qq.com>
2024-01-29 19:50:17 +0800

    fix: lightning broswer module

--- apps/lightning/src/metadata.ts
+++ apps/lightning/src/metadata.ts
@@ -25,6 +25,13 @@ export default async () => {
               take: { required: true, type: () => Number, description: "\u6BCF\u9875\u6570\u91CF" },
               skip: { required: true, type: () => Number, description: "\u8DF3\u8FC7\u6570\u91CF" },
             },
+            PostBrowserMarkBodyDTO: {
+              title: { required: true, type: () => String, description: "\u6807\u9898" },
+              icon: { required: true, type: () => String, description: "\u56FE\u6807" },
+              color: { required: true, type: () => String, description: "\u989C\u8272" },
+              link: { required: true, type: () => String, description: "\u94FE\u63A5" },
+              index: { required: true, type: () => Number, description: "\u7D22\u5F15" },
+            },
           },
         ],
       ],
@@ -43,7 +50,12 @@ export default async () => {
         ],
         [
           import("./modules/broswer/mark.controller"),
-          { BrowserMarkController: { getList: { summary: "\u83B7\u53D6\u5386\u53F2\u8BB0\u5F55", type: Object }, create: { type: Object } } },
+          {
+            BrowserMarkController: {
+              getList: { summary: "\u83B7\u53D6\u6240\u6709\u5386\u53F2\u8BB0\u5F55", type: Object },
+              create: { summary: "\u66F4\u65B0\u6240\u6709\u5386\u53F2\u8BB0\u5F55", type: Object },
+            },
+          },
         ],
       ],
     },
--- apps/lightning/src/modules/broswer/browser.module.ts
+++ apps/lightning/src/modules/broswer/browser.module.ts
@@ -1,11 +1,11 @@
 import { Module } from "@nestjs/common";
 import { BrowserTrackController } from "./track.controller";
-import { BrowserBookMarkRepository, BrowserTrackRepository } from "cc.naily.element.database";
+import { BrowserBookMarkRepository, BrowserTrackRepository, UserRepository } from "cc.naily.element.database";
 import { NailyContext } from "cc.naily.element.shared";
 import { BrowserMarkController } from "./mark.controller";
 
 @Module({
   controllers: [BrowserTrackController, BrowserMarkController],
-  providers: [BrowserTrackRepository, BrowserBookMarkRepository],
+  providers: [BrowserTrackRepository, BrowserBookMarkRepository, UserRepository],
 })
 export class browserTrackModule extends NailyContext {}
